9c2cb94ad43cffce9b18c8d01d305df8109ab1dc
ReN <132862627+ReNlapf10@users.noreply.github.com>
2024-02-02 19:11:16 -0300

    Finish exercises from 1 to 5

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return 'ERROR';
+  }
+  let completeString = '';
+  for (let i = 0; i < num; i++) {
+    completeString += string;
+  }
+  return completeString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  let n1;
+  let n2;
+  if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+    return 'ERROR';
+  }
+  if (a > b) {
+    n1 = b;
+    n2 = a;
+  } else {
+    n1 = a;
+    n2 = b;
+  }
+  for (let i = n1; i <= n2; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
