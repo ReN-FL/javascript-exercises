41f3024afd2e1f877a834593f4d20d08cacb15c4
ReN <132862627+ReNlapf10@users.noreply.github.com>
2024-03-04 17:46:12 -0300

    Finish all exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let strLow = str.toLowerCase();
+  strClean = strLow.replace(/[^a-z0-9]/g, '');
+  let strRev = strClean.split('').reverse().join('');
+  return strClean === strRev;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,27 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  let greatestId;
+  let current = 0;
+  let greatest = 0;
+  arr.forEach((e) => {
+    if (e.yearOfDeath != undefined) {
+      current = e.yearOfDeath - e.yearOfBirth;
+      if (current > greatest) {
+        greatest = current;
+        greatestId = e;
+      }
+    } else {
+      let year = new Date().getFullYear();
+      current = year - e.yearOfBirth;
+      if (current > greatest) {
+        greatest = current;
+        greatestId = e;
+      }
+    }
+  });
+  return greatestId;
 };
+// yearOfBirth:
+// yearOfDeath:
 
 // Do not edit below this line
 module.exports = findTheOldest;
